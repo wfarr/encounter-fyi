142a24bb8d5d2cdfe08890e48bd1817709e01b4a
Will Farrington <wfarrington@heroku.com>
2019-03-25 19:09:49 -0400

    theoretical react-router on heroku support

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
     end
   end
 
+  # route any path we don't recognize to react-router?
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
