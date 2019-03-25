6a3718be80ed02419818266746b87b879e22aec6
Will Farrington <wfarrington@heroku.com>
2019-03-25 18:47:13 -0400

    Move games controller under api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :games
+  namespace :api do
+    namespace :v1 do
+      resources :games
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
