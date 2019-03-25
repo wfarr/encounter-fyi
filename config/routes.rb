4a4025a9b141bd3f20473c0a989a83a6b06b025e
Will Farrington <wfarrington@heroku.com>
2019-03-25 19:57:35 -0400

    Revert "try moving the catch-all route to top"
    This reverts commit bbf9058e74128b788d7869fc53dfe3f630aa8b77.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
-
   namespace :api do
     namespace :v1 do
       resources :games
@@ -11,6 +7,9 @@ Rails.application.routes.draw do
 
   # route any path we don't recognize to react-router?
   root "application#fallback_index_html"
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
