bbf9058e74128b788d7869fc53dfe3f630aa8b77
Will Farrington <wfarrington@heroku.com>
2019-03-25 19:53:06 -0400

    try moving the catch-all route to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
   namespace :api do
     namespace :v1 do
       resources :games
@@ -7,9 +11,6 @@ Rails.application.routes.draw do
 
   # route any path we don't recognize to react-router?
   root "application#fallback_index_html"
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
