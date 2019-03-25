15774b06e7c4186288156fc962826ffdc5fd5d8f
Will Farrington <wfarrington@heroku.com>
2019-03-25 18:44:48 -0400

    Add seed data for games

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+games = Game.create([
+  { name: "Storm King's Thunder" },
+  { name: "Waterdeep: Dragon Heist" },
+  { name: "Critical Role" },
+])
