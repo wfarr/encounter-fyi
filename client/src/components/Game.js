ddc1bb9d12b8806060751e7fe49558e4c6f2fe15
Thomas Holmes <thomas.holmes@salesforce.com>
2019-03-26 14:41:59 -0400

    Load individual games from remote server instead of link state

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Game extends React.Component {
   constructor(props) {
@@ -6,17 +7,40 @@ class Game extends React.Component {
 
     console.log(props);
     this.props = props;
-    this.state = this.props.location.state;
+    this.state = {
+      loaded: false,
+      gameId: this.props.match.params.id
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.game === undefined) {
+      axios
+        .get(`/api/v1/games/${this.state.gameId}`)
+        .then(request => {
+          console.log(request.data);
+          this.setState({ loaded: true, game: request.data });
+        })
+        .catch(error => console.log(error));
+    }
   }
 
   render() {
-    return (
-      <div>
-        <h1>GAME DETAIL</h1>
-        <h2>{this.props.match.params.id}</h2>
-        <h2>{this.state.game.name}</h2>
-      </div>
-    );
+    if (this.state.loaded) {
+      return (
+        <div>
+          <h1>GAME DETAIL</h1>
+          <h2>{this.props.match.params.id}</h2>
+          <h2>{this.state.game.name}</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Loading game...</h3>
+        </div>
+      );
+    }
   }
 }
 
