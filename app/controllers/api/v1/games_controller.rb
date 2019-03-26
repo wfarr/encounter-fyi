6abe97846109e1a25aee244eac6090bd06bb2049
Thomas Holmes <thomas.holmes@salesforce.com>
2019-03-26 14:07:45 -0400

    Use array+key syntax to specify nested resource location

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -19,7 +19,7 @@ module Api::V1
       @game = Game.new(game_params)
 
       if @game.save
-        render json: @game, status: :created, location: api_v1_game_url(@game)
+        render json: @game, status: :created, location: [:api, :v1, @game]
       else
         render json: @game.errors, status: :unprocessable_entity
       end
