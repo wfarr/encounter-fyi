8356730228c7fd552d69a37c4602fd188ba78b8f
Will Farrington <wfarrington@heroku.com>
2019-03-25 20:17:44 -0400

    filter for only html

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root to: "application#fallback_index_html"
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && !request.format.json?
+    !request.xhr? && !request.format.html?
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
