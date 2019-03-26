4bdf10f6a91bf7d4e0a6bd3c727098f2144cb150
Will Farrington <wfarrington@heroku.com>
2019-03-25 20:13:52 -0400

    missing import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Games from './components/Games'
 import NotFound from './components/NotFound'
