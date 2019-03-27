e81ee0d1a14754c04b70b3b16a06215545565822
Will Farrington <wfarrington@heroku.com>
2019-03-27 15:21:14 -0400

    Add persistent character to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :persistent_characters
+
       resources :games do
         resources :encounters
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_202341) do
+ActiveRecord::Schema.define(version: 2019_03_27_190328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,21 @@ ActiveRecord::Schema.define(version: 2019_03_26_202341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "encounters_count", default: 0
+  end
+
+  create_table "persistent_characters", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "strength", default: 1, null: false
+    t.integer "dexterity", default: 1, null: false
+    t.integer "constitution", default: 1, null: false
+    t.integer "intelligence", default: 1, null: false
+    t.integer "wisdom", default: 1, null: false
+    t.integer "charisma", default: 1, null: false
+    t.integer "hit_point_maximum", default: 1, null: false
+    t.integer "hit_points", default: 1, null: false
+    t.integer "speed", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
