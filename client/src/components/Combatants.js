58454ba496f78cf6c16bc11b7fb39d2c12c25484
Thomas Holmes <thomas.holmes@salesforce.com>
2019-03-28 14:33:58 -0400

    Keep and manage all the combatant state at the encounter level

--- client/src/components/Combatants.js
+++ client/src/components/Combatants.js
@@ -1,119 +1,25 @@
 import React from 'react';
 
-import CombatantForm from './CombatantForm';
-
 import './Combatants.scss';
 
 class Combatants extends React.Component {
   constructor(props) {
     super(props);
-
-    console.log('combatants', props);
-    // from parent
-    this.updateHandler = props.updateHandler;
-
-    this.handleForward = this.handleForward.bind(this);
-    this.handleBack = this.handleBack.bind(this);
-    this.handleStart = this.handleStart.bind(this);
-    this.newCombatant = this.newCombatant.bind(this);
-
-    this.state = Object.assign(
-      {
-        combatants: {},
-        order: [],
-        current: null
-      },
-      props.combatants
-    );
-  }
-
-  newCombatant(id, name) {
-    console.log('Adding Character:', id, name);
-    console.log(this.state);
-
-    // Empty checks should be pushed into the form, but the dup check probably should happen here?
-    // Or we could pass a validator down into the form.
-    const errors = {};
-    if (id in this.state.combatants) {
-      errors.id = 'Duplicate ID';
-    }
-    if (id === '') {
-      errors.id = 'ID Must not be empty';
-    }
-    if (name === '') {
-      errors.name = 'Name must not be empty';
-    }
-
-    if ('id' in errors || 'name' in errors) {
-      return errors;
-    }
-
-    const combatants = Object.assign(this.state.combatants, {
-      [id]: { id: id, name: name }
-    });
-    const order = [...this.state.order, id];
-
-    this.setState(
-      {
-        combatants: combatants,
-        order: order
-      },
-      () => {
-        this.updateHandler(this.state);
-      }
-    );
-
-    return errors;
   }
 
   currentActor() {
-    return this.state.combatants[this.order[this.state.currentActor]];
-  }
-
-  advanceTurn(count) {
-    var current = this.state.currentActor + count;
-    const len = this.state.order.length;
-
-    const newPosition = ((current % len) + len) % len;
-
-    this.setState(
-      {
-        currentActor: newPosition
-      },
-      () => this.updateHandler(this.state)
-    );
-  }
-
-  handleForward(e) {
-    console.log(e);
-
-    this.advanceTurn(1);
-
-    e.preventDefault();
-  }
-
-  handleBack(e) {
-    console.log(e);
-
-    this.advanceTurn(-1);
-
-    e.preventDefault();
-  }
-
-  handleStart(e) {
-    console.log(e);
-
-    this.setState({ currentActor: 0 }, () => this.updateHandler(this.state));
+    return this.props.combatants[this.props.order[this.props.currentActor]];
   }
 
   render() {
-    const combatants = this.state.order.map((combatantId, index) => {
-      const combatant = this.state.combatants[combatantId];
+    console.log('***combatants', this.props);
+    const combatants = this.props.order.map((combatantId, index) => {
+      const combatant = this.props.combatants[combatantId];
       return (
         <Combatant
           key={combatant.id}
           combatant={combatant}
-          active={this.state.currentActor == index}
+          active={this.props.currentActor == index}
         />
       );
     });
@@ -121,12 +27,6 @@ class Combatants extends React.Component {
     return (
       <div className="combatants" onKeyDown={this.handleKeyDown}>
         <ul>{combatants}</ul>
-        <button onClick={this.handleBack}>{`<`}</button>
-        <button onClick={this.handleForward}>{`>`}</button>
-        <button onClick={this.handleStart}>Start</button>
-        <div>
-          <CombatantForm createHandler={this.newCombatant} />
-        </div>
       </div>
     );
   }
--- client/src/components/Encounter.js
+++ client/src/components/Encounter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import Combatants from './Combatants';
+import CombatantForm from './CombatantForm';
 
 import './Encounter.scss';
 
@@ -9,17 +10,20 @@ class Encounter extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleForward = this.handleForward.bind(this);
+    this.handleBack = this.handleBack.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.newCombatant = this.newCombatant.bind(this);
+
     this.updateChildState = this.updateChildState.bind(this);
     this.props = props;
     this.state = {
       loaded: false,
       id: this.props.match.params.id,
-      gameId: this.props.match.params.game_id,
-      encounter: {
-        state: {
-          combatants: {}
-        }
-      }
+      game_id: this.props.match.params.game_id,
+      combatants: {},
+      currentActor: 0,
+      order: []
     };
   }
 
@@ -27,7 +31,7 @@ class Encounter extends React.Component {
     console.log('Storing state', this.state.encounter);
     axios
       .patch(`/api/v1/encounters/${this.state.id}`, {
-        encounter: { state: this.state.encounter.state }
+        encounter: { state: this.state }
       })
       .then(response => {
         console.log('patch response', response);
@@ -41,8 +45,8 @@ class Encounter extends React.Component {
         .get(`/api/v1/encounters/${this.state.id}`)
         .then(request => {
           console.log('reloaded state', request.data);
-          this.setState({ loaded: true, encounter: request.data });
-          console.log(this.state);
+          this.setState({ ...request.data.state, loaded: true });
+          console.log('final state', this.state);
         })
         .catch(error => console.log(error));
     }
@@ -53,27 +57,100 @@ class Encounter extends React.Component {
     this.setState({ encounter: { state: { [key]: obj } } });
   }
 
+  newCombatant(id, name) {
+    console.log('Adding Character:', id, name);
+    console.log(this.state);
+
+    // Empty checks should be pushed into the form, but the dup check probably should happen here?
+    // Or we could pass a validator down into the form.
+    const errors = {};
+    if (id in this.state.combatants) {
+      errors.id = 'Duplicate ID';
+    }
+    if (id === '') {
+      errors.id = 'ID Must not be empty';
+    }
+    if (name === '') {
+      errors.name = 'Name must not be empty';
+    }
+
+    if ('id' in errors || 'name' in errors) {
+      return errors;
+    }
+
+    const combatants = Object.assign(this.state.combatants, {
+      [id]: { id: id, name: name }
+    });
+    const order = [...this.state.order, id];
+
+    this.setState({
+      combatants: combatants,
+      order: order
+    });
+
+    return errors;
+  }
+
+  advanceTurn(count) {
+    var current = this.state.currentActor + count;
+    const len = this.state.order.length;
+
+    const newPosition = ((current % len) + len) % len;
+
+    this.setState({ currentActor: newPosition });
+  }
+
+  handleForward(e) {
+    console.log(e);
+
+    this.advanceTurn(1);
+
+    e.preventDefault();
+  }
+
+  handleBack(e) {
+    console.log(e);
+
+    this.advanceTurn(-1);
+
+    e.preventDefault();
+  }
+
+  handleStart(e) {
+    console.log(e);
+
+    this.setState({ currentActor: 0 });
+  }
+
   render() {
     if (this.state.loaded) {
       return (
         <div>
-          <h1>ENCOUNTER DETAIL</h1>
           <dl>
-            {this.state.encounter.game_id ? (
+            {this.state.game_id ? (
               <div>
                 <dt>Game ID:</dt>
-                <dd>{this.state.encounter.game_id}</dd>
+                <dd>{this.state.game_id}</dd>
               </div>
             ) : null}
             <dt>Encounter ID:</dt>
             <dd>{this.state.id}</dd>
             <dt>Name</dt>
-            <dd>{this.state.encounter.name}</dd>
+            <dd>{this.state.name}</dd>
             <div className="container">
               <Combatants
-                combatants={this.state.encounter.state.combatants}
-                updateHandler={s => this.updateChildState('combatants', s)}
+                combatants={this.state.combatants}
+                order={this.state.order}
+                currentActor={this.state.currentActor}
               />
+              <div>
+                <button onClick={this.handleBack}>{`<`}</button>
+                <button onClick={this.handleForward}>{`>`}</button>
+                <button onClick={this.handleStart}>Start</button>
+                <div>
+                  <CombatantForm createHandler={this.newCombatant} />
+                </div>
+              </div>
             </div>
           </dl>
         </div>
